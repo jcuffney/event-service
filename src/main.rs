76a5527018f4abea7abe6270feb774cc047c941f
Joe Cuffney <josephcuffney@gmail.com>
2024-11-05 11:20:25 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,37 @@
-// lambda entry point
-fn handler() {
-    println!("Hello, lambda!");
+use lambda_runtime::{service_fn, tracing, Error, LambdaEvent};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+struct Request {
+    command: String,
 }
 
-// local entry point
-fn main() {
-    handler();
+#[derive(Serialize)]
+struct Response {
+    req_id: String,
+    msg: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // required to enable CloudWatch error logging by the runtime
+    tracing::init_default_subscriber();
 
-    #[test]
-    fn test_handler() {
-        handler();
-    }
+    let func = service_fn(handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+pub(crate) async fn handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
+    // extract some useful info from the request
+    let command = event.payload.command;
+
+    // prepare the response
+    let resp = Response {
+        req_id: event.context.request_id,
+        msg: format!("Command {} executed.", command),
+    };
+
+    // return `Response` (it will be serialized to JSON automatically by the runtime)
+    Ok(resp)
 }
